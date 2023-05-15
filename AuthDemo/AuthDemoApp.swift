9085b9a077377de25c144765fe200bf09c648b5e
KaitoSeita <113151647+KaitoSeita@users.noreply.github.com>
2023-05-15 16:15:35 +0900

    Initial Commit

---
+++ AuthDemo/AuthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthDemoApp.swift
+//  AuthDemo
+//
+//  Created by セイタカイト on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct AuthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
